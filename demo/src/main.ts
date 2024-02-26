124842072c09ef80bda0afc08db79a67b4e7b5b7
Cwd295645351 <295645351@qq.com>
2024-02-26 14:20:57 +0800

    feat: 修改 demo 结构

--- demo/src/main.ts
+++
@@ -1,5 +0,0 @@
-// demo/src/main.ts
-import { createApp } from "vue"
-import App from "./App.vue"
-
-createApp(App).mount("#app")
---
+++ demos/demo1/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// demo/src/App.vue
+import { Input, Button } from '@chenwdui/ui';
+</script>
+
+<template>
+  <div>
+    <Button>1111</Button>
+    <Input />
+  </div>
+</template>
