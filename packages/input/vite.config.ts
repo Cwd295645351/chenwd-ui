31ba0169b7f288f0a8b7a94ac496d8463532cb84
Cwd295645351 <295645351@qq.com>
2024-04-08 12:44:39 +0800

    feat: 定制组件库的打包体系

--- packages/input/vite.config.ts
+++ packages/input/vite.config.ts
@@ -1,23 +1,4 @@
-// packages/input/vite.config.ts
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+// packages/build/vite.config.ts
+import { generateVueConfig } from "../build/build.config"
 
-export default defineConfig({
-  // 增加插件的使用
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: './src/index.ts',
-      name: 'ChenwduiInput',
-      fileName: 'chenwdui-input',
-    },
-    minify: false,
-    rollupOptions: {
-      external: [
-        // 除了 @chenwdui/shared，未来可能还会依赖其他内部模块，不如用正则表达式将 @chenwdui 开头的依赖项一起处理掉
-        /@chenwdui.*/,
-        'vue',
-      ],
-    },
-  },
-});
+export default generateVueConfig()
